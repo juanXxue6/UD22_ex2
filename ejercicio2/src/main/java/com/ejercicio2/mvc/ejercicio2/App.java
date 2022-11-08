ef17405171debcb31b042331eac034e03cc3eef2
juanXxue6 <juanxue6@gmail.com>
2022-11-08 10:21:05 +0100

    commit inicio proyecto

---
+++ ejercicio2/src/main/java/com/ejercicio2/mvc/ejercicio2/App.java
@@ -0,0 +1,13 @@
+package com.ejercicio2.mvc.ejercicio2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
